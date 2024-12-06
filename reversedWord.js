bf05510566aff8cf4b48994e5774be19e2bd82e2
VladosDeveloper <yurenya.vlad.01@mail.ru>
2024-12-06 17:37:51 +0300

    Smallest Number

--- reversedWord.js
+++ reversedWord.js
@@ -1,3 +1,5 @@
+
+// Reverse entire string
 function solution(str) {
 	let reversedText = str
 	reversedText = reversedText.split('').reverse().join('')
@@ -6,9 +8,33 @@ function solution(str) {
 
 let text = solution('world')
 
+// convert integer to string
 function numberToString(num) {
 	let covertedString = String(num)
 	return covertedString
 }
 
-let resolve = numberToString(1234)
+
+//Finding positive number of array
+function positiveSum(arr) {
+	let emptyArray = arr
+	let sumArray = 0
+	for (const numberOfArray of emptyArray) {
+		if (numberOfArray >= 0) {
+			sumArray += numberOfArray
+		} else {
+			continue
+		}
+	}
+	return sumArray
+}
+
+let arraySum = positiveSum([1, 2, 3, 4])
+
+// finding min number in array
+function findSmallestInt(arr) {
+	let smallestInt = Math.min(...arr)
+	return smallestInt
+}
+
+let smallestInt = findSmallestInt([4, 10, 15, 6, 1])
