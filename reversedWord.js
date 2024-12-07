528e37865b18d2de352b68597cc25115677da478
VladosDeveloper <yurenya.vlad.01@mail.ru>
2024-12-07 21:05:03 +0300

    rps

--- reversedWord.js
+++ reversedWord.js
@@ -38,3 +38,20 @@ function findSmallestInt(arr) {
 }
 
 let smallestInt = findSmallestInt([4, 10, 15, 6, 1])
+
+
+const rps = (p1, p2) => {
+	if (p1 === 'scissors' && p2 === 'paper') {
+		return 'Player 1 won!'
+	} else if (p1 === 'paper' && p2 === 'rock') {
+		return 'Player 1 won!'
+	} else if (p1 === 'rock' && p2 === 'scissors') {
+		return 'Player 1 won!'
+	} else if (p1 === p2) {
+		return 'Draw!'
+	} else {
+		return 'Player 2 won!'
+	}
+}
+
+rps('paper', 'paper')
